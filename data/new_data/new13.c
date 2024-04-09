771e911c5efaf64c1927639d389ba1d013f66a7e
jiahaogai <jxg053@gmail.com>
2024-04-09 23:31:34 +0800

    new data source files uploaded

---
+++ data/new_data/new13.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <math.h>
+
+#define N 64
+
+#define DATA_TYPE float
+#define SCALAR_VAL(x) x##f
+#define SQRT_FUN(x) sqrtf(x)
+#define EXP_FUN(x) expf(x)
+#define POW_FUN(x,y) powf(x,y)
+
+void syrk(DATA_TYPE alpha, DATA_TYPE beta, DATA_TYPE A[N][N], DATA_TYPE B[N][N], DATA_TYPE C_out[N][N]);
+
+
+void syrk(DATA_TYPE alpha, DATA_TYPE beta, DATA_TYPE A[N][N], DATA_TYPE B[N][N], DATA_TYPE C_out[N][N]) {
+	int i, j, k;
+
+	DATA_TYPE buff_A0[N][N], buff_A1[N][N];
+	DATA_TYPE buff_B[N][N];
+	DATA_TYPE buff_C_out[N][N];
+
+	lprd_1: for (i = 0; i < N; i++) {
+		lprd_2: for (j = 0; j < N; j++) {
+			buff_A0[i][j] = A[i][j];
+			buff_A1[i][j] = A[i][j];
+			buff_B[i][j] = B[i][j];
+			buff_C_out[i][j] = 0;
+		}
+	}
+
+	lp1: for (i = 0; i < N; i++) {
+		lp2: for (j = 0; j < N; j++) {
+			// if (j > i) continue;
+			lp3: for (k = 0; k < N; k++) {
+				buff_C_out[i][j] += alpha * buff_A0[i][k] * buff_A1[j][k];
+			}
+		}
+	}
+
+	lp4: for (i = 0; i < N; i++) {
+		lp5: for (j = 0; j < N; j++) {
+			// if (j > i) continue;
+			buff_C_out[i][j] += beta * buff_B[i][j];
+		}
+	}
+
+	
+
+	lpwr_1: for(i = 0; i < N; i++) {
+		lpwr_2: for(j = 0; j < N; j++) {
+			if (j > i) C_out[i][j] = 0;
+			else C_out[i][j] = buff_C_out[i][j];
+		}
+	}
+}
+
