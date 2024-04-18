29ab081a8fb1ad2d47660dff17616c3dce48a147
jiahaogai <jxg053@gmail.com>
2024-04-19 02:58:10 +0800

    inference.sh uploaded

--- inference.sh
+++ inference.sh
@@ -1,3 +1,20 @@
-cd axolotl
-python -m axolotl.cli.inference examples/code-llama/7b/qlora.yml --lora_model_dir="./qlora-out"
-cd ..
+#!/bin/bash
+
+# 检查参数个数是否为1
+#if [ "$#" -ne 1 ]; then
+#  echo "Usage: $0 [-syntax_feedback | -functionality_feedback]"
+#  exit 1
+#fi
+
+# 根据参数调用不同的Python脚本
+case "$1" in
+  -syntax_feedback)
+    python fd_loop/fd_loop.py
+    ;;
+  -functionality_feedback)
+    python fd_loop/fd_loop_fc.py
+    ;;
+  *)
+    python vllm_inference.py
+    ;;
+esac
