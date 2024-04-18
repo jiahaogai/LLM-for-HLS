a61e8587eb2867e09b0e52509a52768f7ad324c7
jiahaogai <jxg053@gmail.com>
2024-04-18 17:44:54 +0800

    syntax_check updated

--- syntax_check.py
+++ syntax_check.py
@@ -11,19 +11,27 @@ async def check_c_file_syntax_async(file_path):
 
     stdout, stderr = await process.communicate()
 
-    # Return True if no syntax errors, False otherwise
-    return process.returncode == 0
+    # if process.returncode != 0:
+    #     return file_path, stderr.decode()
+    # else:
+    #     return file_path, "No syntax errors found."
+    return process.returncode == 0  # True if no syntax errors
 
 
 async def main():
-    folder_path = "/root/autodl-tmp/LLM-for-HLS/test_output"  # './outputs'
-    # folder_path = "/root/autodl-tmp/LLM-for-HLS/test_output_with_feedback_loop"
-    pass_num = len([1 for file in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, file))])
+    folder_path = "/root/autodl-tmp/LLM/LLM-for-HLS/test_output"  # './outputs'
+    # folder_path = "/root/autodl-tmp/LLM/LLM-for-HLS/test_output_with_feedback_loop"  # './outputs'
+    pass_num = len(os.listdir(os.path.join(folder_path, os.listdir(folder_path)[0])))
     print(f"Pass num: {pass_num}")
     c_file_dirs = [file_dir for file_dir in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, file_dir)) and
-                   file_dir.startswith("test")]
+                   ".c" in file_dir]
     print(f"C file dirs: {c_file_dirs}")
-    
+    # tasks = [any([check_c_file_syntax_async(os.path.join(folder_path, file)) for file in file_dir]) for file_dir in
+    #          c_file_dirs]
+    # tasks = [check_c_file_syntax_async(os.path.join(folder_path, file))
+    #          for file_dir in c_file_dirs
+    #          for file in os.listdir(os.path.join(folder_path, file_dir))]
+    # results = await asyncio.gather(*tasks)
     results = []
     for file_dir in c_file_dirs:
         dir_results = await asyncio.gather(*[check_c_file_syntax_async(os.path.join(folder_path, file_dir, file))
@@ -31,11 +39,13 @@ async def main():
         print(f"Dir results: {dir_results}")
         results.append(1 if any(dir_results) else 0)
 
-    # Calculate the number of files with syntax errors
+    # for file, result in results:
+    #     print(f"File: {file}\n{result}\n")
+    # 计算语法错误的文件数
     print(f"Total test instances: {len(results)}")
     print(f"Total pass instances: {sum(results)}")
     print(f"Total fail instances: {len(results) - sum(results)}")
     print(f"Pass rate: {sum(results) / len(results) * 100}%")
 
-# Run the asynchronous main function
+# 运行异步主函数
 asyncio.run(main())
