698db247ac59b303425788f84b56c83fda84aab9
jiahaogai <jxg053@gmail.com>
2024-04-19 16:52:53 +0800

    comments changed to En

--- inference.sh
+++ inference.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# 检查参数个数是否为1
-#if [ "$#" -ne 1 ]; then
-#  echo "Usage: $0 [-syntax_feedback | -functionality_feedback]"
-#  exit 1
-#fi
+# check if the number of arguments is 1
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 [-syntax_feedback | -functionality_feedback]"
+  exit 1
+fi
 
-# 根据参数调用不同的Python脚本
 case "$1" in
   -syntax_feedback)
     python fd_loop/fd_loop.py
