f6b444db1876f70bcccf2f109c071fa3dfc132bd
Jiahao Gai <jxg053@gmail.com>
2024-05-20 18:35:38 +0800

    update functionality_check.sh

--- functionality_check.sh
+++ functionality_check.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
-# 检查参数个数是否为2
+# Check if the number of parameters is 2
 if [ "$#" -ne 2 ]; then
   echo "Usage: $0 [-woCot | -cot] [-woFd | -synFd | -funFd]"
   exit 1
 fi
 
-# 将参数组合成一个字符串
+# Combine parameters into a single string
 args="$1_$2"
 
-# 根据参数调用不同的Python脚本
+# Call different Python scripts based on the parameters
 case "$args" in
   -woCot_-woFd)
-    python functionality_check_woCot_woFd.py
+    python ./src/func_checkfunctionality_check_woCot_woFd.py
     ;;
   -woCot_-synFd)
-    python functionality_check_woCot_synFd.py
+    python ./src/func_checkfunctionality_check_woCot_synFd.py
     ;;
   -woCot_-funFd)
-    python functionality_check_woCot_funFd.py
+    python ./src/func_checkfunctionality_check_woCot_funFd.py
     ;;
   -cot_-woFd)
-    python functionality_check_cot_woFd.py
+    python ./src/func_checkfunctionality_check_cot_woFd.py
     ;;
   -cot_-synFd)
-    python functionality_check_cot_synFd.py
+    python ./src/func_checkfunctionality_check_cot_synFd.py
     ;;
   -cot_-funFd)
-    python functionality_check_cot_funFd.py
+    python ./src/func_checkfunctionality_check_cot_funFd.py
     ;;
   *)
     echo "Invalid argument combination: $1 $2"
