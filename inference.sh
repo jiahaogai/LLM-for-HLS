520219b61a00e1ef3995e619b7be241ad0ce00b1
Jiahao Gai <jxg053@gmail.com>
2024-05-20 18:36:06 +0800

    update inference.sh

--- inference.sh
+++ inference.sh
@@ -5,22 +5,22 @@ export TOKENIZERS_PARALLELISM=true
 case "$1" in
   -syntax_feedback)
     if [ "$2" = "-cot" ]; then
-        python fd_loop/inference_cot_synFd.py
+        python src/fd_loop/inference_cot_synFd.py
     else
-        python fd_loop/inference_woCot_synFd.py
+        python src/fd_loop/inference_woCot_synFd.py
     fi
     ;;
   -functionality_feedback)
     if [ "$2" = "-cot" ]; then
-        python fd_loop/inference_cot_funFd.py
+        python src/fd_loop/inference_cot_funFd.py
     else
-        python fd_loop/inference_woCot_funFd.py
+        python src/fd_loop/inference_woCot_funFd.py
     fi
     ;;
   -cot)
-    python vllm_inference_cot.py
+    python src/fd_loop/vllm_inference_cot.py
     ;;
   *)
-    python vllm_inference_without_cot.py
+    python src/fd_loop/vllm_inference_without_cot.py
     ;;
 esac
