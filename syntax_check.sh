be1ce811dbdfcb22c0a9ffeb48c4c2615780fc33
Jiahao Gai <jxg053@gmail.com>
2024-05-20 18:43:59 +0800

    update syntax_check.sh

--- syntax_check.sh
+++ syntax_check.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# 检查参数个数是否为2
+# Check if the number of parameters is 2
 if [ "$#" -ne 2 ]; then
   echo "Usage: $0 [-woCot | -cot] [-woFd | -synFd | -funFd]"
   exit 1
 fi
 
-# 将参数组合成一个字符串
+# Combine parameters into one string
 args="$1_$2"
 
-# 根据参数调用不同的Python脚本
+# Call different Python scripts based on the parameters
 case "$args" in
   -woCot_-woFd)
     python ./src/syntax_check/syntax_check_woCot_woFd.py
